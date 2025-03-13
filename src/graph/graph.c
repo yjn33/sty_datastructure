e6cd2e50a65c7dd09e72aec442a633bc999e4dc7
yjn33 <yjn0032@gmail.com>
2025-03-13 18:33:08 +0900

    feat(feat/graph): 그래프 init 함수 구현
    그래프 초기화 및 할당 메모리 주소 반환

--- src/graph/graph.c
+++ src/graph/graph.c
@@ -4,5 +4,29 @@
 #include "graph.h"
 
 
+/*
+    그래프 클래스 초기화 및 메모리 주소 반환 함수
 
+*/
+GraphType *graph_init(void)
+{
+    GraphType *new_graph = malloc(sizeof(GraphType));
+    
+    if(new_graph == NULL_PTR)
+    {
+        printf("fail to malloc \n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    new_graph -> n = 0;
+    
+    for(int v = 0; v < MAX_VERTICES; v += 1)
+    {
+        new_graph -> adj_list[v] = NULL_PTR;
+    }
+
+    return new_graph;
+
+}
 
--- src/graph/graph.h
+++ src/graph/graph.h
@@ -1,3 +1,24 @@
 #define NULL_PTR ((void*)0)
+#define MAX_VERTICES 50
+
+/*
+    그래프 노드 정의
+*/
+typedef struct graph_node
+{
+    int vertex;
+    struct graph_node *link;
+}GraphNode;
+
+/*
+    그래프 타입 클래스 정의
+*/
+typedef struct graph_type
+{
+    int n; // 현재 정점 vertices의 개수
+    GraphNode *adj_list[MAX_VERTICES];
+}GraphType;
+
+
 
 
